6c1899a55531bb7e6bc540665713d102a3939032
level6 <12127151+lleevveell66@users.noreply.github.com>
2025-12-06 13:31:11 -0600

    Just pushing code with no pipeline runs...

--- GetWeather.sh
+++ GetWeather.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-wget -q -N http://192.168.44.55/BERTHA/HyperClock/AstralData.txt -O /usr/local/HyperClock/AstralDataNew.txt
+wget -q -N http://10.10.41.155/BERTHA/HyperClock/AstralData.txt -O /usr/local/HyperClock/AstralDataNew.txt
 data=`cat /usr/local/HyperClock/AstralDataNew.txt | grep forecast`
 if [ -z "$data" ]; then
  exit
