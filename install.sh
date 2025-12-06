2e34457da84dd1bf878e0b217b808a41b5143080
level6 <12127151+lleevveell66@users.noreply.github.com>
2025-12-05 18:11:57 -0600

    Just pushing code with no pipeline runs...

--- install.sh
+++ install.sh
@@ -1,23 +1,29 @@
 #!/bin/sh
 ##################################################################
-# HyperClock 3.0 by level6
+# HyperClock 4.0 by level6
 # https://github.com/lleevveell66/HyperClock
 ##################################################################
 # Find complete instructions on the HyperClock page.
 ##################################################################
 
-echo "Installing HyperClock ..."
+echo "Installing HyperClock:"
+echo " - Changing file modes of executables"
 /bin/chmod 755 HyperClock
 /bin/chmod 755 GetIndoorTemp.sh
 /bin/chmod 755 GetWeather.sh
+echo " - Making the /usr/local/HyperClock/ directory"
 /bin/mkdir -p /usr/local/HyperClock/
 
+echo " - Copying everything here into /usr/local/HyperClock/"
 /bin/cp -a . /usr/local/HyperClock/
 
 echo "Done!"
-echo "  Remember to edit /usr/local/HyperClock/HyperClock.conf to customize your HyperClock"
+echo ""
+echo "  Remember to edit /usr/local/HyperClock/HyperClock.conf to customize your HyperClock."
+echo ""
 echo "  To run it:  /usr/local/HyperClock/HyperClock"
-echo "  See the webpage for other instructions (making it run upon boot on an RPI, etc.):"
+echo ""
+echo "  See the following webpage for other instructions (making it run upon boot on an RPI, etc.):"
 echo "    https://github.com/lleevveell66/HyperClock"
 
 exit 0
