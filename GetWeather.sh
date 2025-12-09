a1d6225a8dd21823ec7b09bcbe18dead6f999502
level6 <12127151+lleevveell66@users.noreply.github.com>
2025-12-09 01:19:54 -0600

    Just pushing code with no pipeline runs...

--- GetWeather.sh
+++ GetWeather.sh
@@ -1,15 +1,38 @@
 #!/bin/sh
+#######################################################################
+# GetWeather.sh v4.0 by level6 of LIE
+#######################################################################
 
+
+#######################################################################
 # Uncomment this section to grab the data from a central server:
-#wget -q -N http://10.10.41.155/BERTHA/HyperClock/AstralData.txt -O /usr/local/HyperClock/AstralDataNew.txt
-#data=`cat /usr/local/HyperClock/AstralDataNew.txt | grep forecast`
-#if [ -z "$data" ]; then
-# exit
-#else
-# /bin/cp /usr/local/HyperClock/AstralDataNew.txt /usr/local/HyperClock/AstralData.txt
-#fi
+#######################################################################
+#
+# URL="http://my-weather-host.com/RemoteStagingDir/weather_data.json"
+# # Stage the downloaded file into tmp/ dir:
+# wget -q -N ${URL} -O /usr/local/HyperClock/tmp/weather_data.json
+#
+#######################################################################
 
-# Uncomment this section to use the OpenWeatherMap API to gather weather data:
+# or...
+
+#######################################################################
+# Uncomment this section to use the OpenWeatherMap API to gather weather data, locally:
+#######################################################################
+
+/usr/local/HyperClock/owm_tools/get_owm_weather > /usr/local/HyperClock/tmp/weather_data_json
+
+#######################################################################
+
+
+
+# Check for an empty or corrupt file, or copy into place if good:
+data=$(/usr/bin/cat /usr/local/HyperClock/tmp/weather_data.json | /usr/bin/grep weather)
+if [ -z "$data" ]; then
+  exit
+else
+  /usr/bin/cp /usr/local/HyperClock/tmp/weather_data.json /usr/local/HyperClock/data/weather_data.json
+fi
 
 
 exit 0
