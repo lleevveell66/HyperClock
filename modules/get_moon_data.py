4e78865bb45fc07acb11d4a294f96c531004fdc5
level6 <12127151+lleevveell66@users.noreply.github.com>
2025-12-15 14:58:04 -0600

    Just pushing code with no pipeline runs...

--- modules/get_moon_data.py
+++ modules/get_moon_data.py
@@ -32,4 +32,4 @@ def calculate_moonrise_moonset(latitude, longitude):
     moonrise = observer.previous_rising(moon, start=now)
     moonset = observer.next_setting(moon, start=now)
 
-    return (moonrise.strftime("%I:%M %p"), moonset.strftime("%I:%M %p"))
+    return (moonrise.strptime("%I:%M %p"), moonset.strtime("%I:%M %p"))
